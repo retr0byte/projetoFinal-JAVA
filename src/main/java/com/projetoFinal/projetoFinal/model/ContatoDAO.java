1de482f57c2bc7bb50331afad3091c0c87832d6a
gabriielsilva5 <dev.gbcproject@gmail.com>
2021-11-18 07:27:21 -0300

    classes DAO

---
+++ src/main/java/com/projetoFinal/projetoFinal/model/ContatoDAO.java
@@ -0,0 +1,23 @@
+package com.projetoFinal.projetoFinal.model;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContatoDAO {
+
+	@Autowired
+	DataSource dataSource;
+	
+	JdbcTemplate jdbc;
+	
+	@PostConstruct
+	private void initialize() {
+		jdbc = new JdbcTemplate(dataSource);
+	}
+	
+}
