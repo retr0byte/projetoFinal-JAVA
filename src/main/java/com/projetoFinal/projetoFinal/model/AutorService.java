02f2d3ef7080fbaa50cbe0c3ad4646cdf0f80981
gabriielsilva5 <dev.gbcproject@gmail.com>
2021-11-18 07:34:00 -0300

    classes Service

---
+++ src/main/java/com/projetoFinal/projetoFinal/model/AutorService.java
@@ -0,0 +1,12 @@
+package com.projetoFinal.projetoFinal.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+
+	@Autowired
+	AutorDAO adao;
+	
+}
